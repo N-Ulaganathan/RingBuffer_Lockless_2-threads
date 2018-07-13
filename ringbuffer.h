105042ec2f7a6033d8bb1e2a309d5f335cf6c76d
Ulaganathan Natarajan <ulaganathan.n@hotmail.com>
2018-07-13 15:43:38 +0530

    Create ringbuffer.h

---
+++ ringbuffer.h
@@ -0,0 +1,61 @@
+#ifndef RINGBUFFER_H_INCLUDED
+#define RINGBUFFER_H_INCLUDED
+
+typedef struct message
+{
+    char buffer[1024];
+    int read_flag;
+} message;
+
+typedef struct ring_buffer
+{
+    struct message* messages;
+    int max_size;
+    int size;
+    int sequence;
+    int head;
+    int tail;
+} ring_buffer;
+
+void rb_init(ring_buffer* rb, size_t max_size)
+{
+    if(!rb)
+    {
+        printf("alloc memory for ring buffer first\n");
+        exit(-1);
+    }
+    rb->messages = (message*)malloc(max_size * sizeof(message));
+    if(rb->messages == NULL)
+    {
+        printf("Unable to alloc memory for ring buffer messages\n");
+        exit(-1);
+    }
+    rb->max_size=max_size;
+    rb->size = rb->sequence = 0;
+    rb->head = rb->tail = -1;
+}
+
+int rb_write(ring_buffer* rb, const char* buffer)
+{
+    rb->head = (rb->head+1) % rb->max_size;
+    while(rb->messages[rb->head].read_flag==1)
+        rb->head = (rb->head+1) % rb->max_size;
+    sprintf(rb->messages[rb->head].buffer, "%s", buffer);
+    rb->messages[rb->head].read_flag=1;
+    rb->size++;
+    rb->sequence++;
+    return 0;
+}
+
+int rb_read(ring_buffer* rb, char* buffer)
+{
+    rb->tail = (rb->tail+1) % rb->max_size;
+    while(rb->messages[rb->tail].read_flag!=1)
+        rb->tail = (rb->tail+1) % rb->max_size;
+    sprintf(buffer, "%s", rb->messages[rb->tail].buffer);
+    rb->messages[rb->tail].read_flag=2;
+    rb->size--;
+    return 0;
+}
+
+#endif // RINGBUFFER_H_INCLUDED
